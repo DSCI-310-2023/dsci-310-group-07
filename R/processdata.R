d77414cb3caaefd808a65f05ccde8da6576451a6
Harbor Zhang <harborz2000@gmail.com>
2023-03-08 15:31:30 -0800

    correct the tests for processdata after #na.omit

--- R/processdata.R
+++ R/processdata.R
@@ -1,7 +1,7 @@
 # author: Haobo (Harbor) Zhang
 # date: 2023-03-07
 
-## take the given dataset, remove the last column and the missing values.
+## take the given dataset, remove the last column (and the missing values).
 
 processdata <- function(dataset) {
   dataset = dataset[,-length(dataset)] #%>%
--- tests/processdata-test.R
+++ tests/processdata-test.R
@@ -1,4 +1,4 @@
 library(testthat)
 
 expect_equal(colnames(processdata(df)[,26]),"price")
-expect_equal(nrow(processdata(df)[,26]),160)
+expect_equal(nrow(processdata(df)[,26]),205)
