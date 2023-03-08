dce47e4cda1eb39900b46f823410879f32f31784
Harbor Zhang <harborz2000@gmail.com>
2023-03-07 20:24:08 -0800

    write processdata function

--- R/processdata.R
+++ R/processdata.R
@@ -1,1 +1,9 @@
-11
+# author: Haobo (Harbor) Zhang
+# date: 2023-03-07
+
+## take the given dataset, remove the last column and the missing values.
+
+processdata <- function(dataset) {
+  df = dataset[,-length(dataset)] %>%
+    na.omit(dataset)
+}
