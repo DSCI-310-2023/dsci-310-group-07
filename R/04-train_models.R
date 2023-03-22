37669d53a428a6e64bdfa58d9814780c6f284bc9
Jiaying Liao <jiaying.liao@qq.com>
2023-03-22 16:20:50 -0700

    fix gramma in report

--- R/04-train_models.R
+++ R/04-train_models.R
@@ -14,9 +14,35 @@ source(here("R/02-read_data.R"))
 
 # generate the traning and testing sets from `get_trt_test()` function
 tst<-get_tr_tst(automobile)
+
 training_df<-get_tr_tst(automobile,"basic")[[1]]
 testing_df<-get_tr_tst(automobile,"basic")[[2]] 
+
 training_df_at<-get_tr_tst(automobile,"at")[[1]]
 testing_df_at<-get_tr_tst(automobile,"at")[[2]]
 
+training_df_sub<-get_tr_tst(automobile,"sub")[[1]]
+testing_df_sub<-get_tr_tst(automobile,"sub")[[2]] 
+
+# prepare x, y matrice for lasso and ridge regression
+x_train_mat<-get_trm_tsm(training_df_sub,testing_df_sub,set="training")[[1]]
+y_train_mat<-get_trm_tsm(training_df_sub,testing_df_sub,set="training")[[2]]
+x_test_mat<-get_trm_tsm(training_df_sub,testing_df_sub,set="testing")[[1]]
+y_test_mat<-get_trm_tsm(training_df_sub,testing_df_sub,set="testing")[[2]]
+
+# Lasso regression
+## plot 
+lasso_plot <- get_model_plot(x_train_mat,y_train_mat,model="lasso",ask="plot")
+## model
+lasso_mods<-get_model_plot(x_train_mat,y_train_mat,model="lasso",ask="modeling")
+lasso_mod<-lasso_mods[[1]]
+lasso_mod_1se<-lasso_mods[[2]]
+
+# Ridge regression
+## plot
+ridge_plot <- get_model_plot(x_train_mat,y_train_mat,model="ridge",ask="plot")
+## model
+ridge_mods<-get_model_plot(x_train_mat,y_train_mat,model="ridge",ask="modeling")
+ridge_mod<-ridge_mods[[1]]
+ridge_mod_1se<-ridge_mods[[2]]
 
