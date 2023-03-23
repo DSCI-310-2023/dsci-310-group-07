a2a9f7cea700e4b7639ab8b3a86c85ca207e4af3
Jiaying Liao <jiaying.liao@qq.com>
2023-03-23 11:25:00 -0700

    change function to forloop

--- R/04-eda_plots.R
+++ R/04-eda_plots.R
@@ -6,6 +6,7 @@
 library(tidyverse)
 library(here)
 library(cowplot)
+library(testthat)
 
 source(here("R/02-read_data.R"))
 source(here("R/03-eda_tables.R"))
@@ -15,30 +16,31 @@ source(here("R/plotAll.R"))
 # obtain plots from `plotAll` function
 plots <- plotAll(automobile, nms)
 
-# This function will save the bar plot to figs
-save_single_plot <- function(pl, name) {
+# This will save the bar plot to figs
+
+bar_idx <- c(1,8)
+bar_names <- c("make.png","cld.png")
+
+for (i in 1:2) {
+  idx <- bar_idx[i]
+  name <- bar_names[i]
   ggsave(name,
-         plot = pl,
+         plot = plots[[idx]],
          path = here("analysis/figs"),
          width = 5, height = 3)
 }
 
-save_single_plot(plots[[1]],"make.png")
-save_single_plot(plots[[8]],"cld.png")
+# This will combine an histogram and scatterplot, and save it in figs
+combine_idx <- c(2,4,6,9,11,13)
+combine_names <- c("length.png","width.png","curb_weight.png",
+                   "eg_size.png","horse_pw.png","hw_mpg.png")
 
-# This function will combine an histogram and scatterplot, and save it in figs
-save_combine_plot <- function(pl1,pl2,name) {
+for (i in 1:6) {
+  idx <- combine_idx[i]
+  name <- combine_names[i]
   ggsave(name,
-         plot = plot_grid(pl1,pl2),
+         plot = plot_grid(plots[[idx]],plots[[idx+1]]),
          path = here("analysis/figs"),
          width = 6, height = 2.5)
+  
 }
-
-save_combine_plot(plots[[2]],plots[[3]],"length.png")
-save_combine_plot(plots[[4]],plots[[5]],"width.png")
-save_combine_plot(plots[[6]],plots[[7]],"curb_weight.png")
-save_combine_plot(plots[[9]],plots[[10]],"eg_size.png")
-save_combine_plot(plots[[11]],plots[[12]],"horse_pw.png")
-save_combine_plot(plots[[13]],plots[[14]],"hw_mpg.png")
-
-
